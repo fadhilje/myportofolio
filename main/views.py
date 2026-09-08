9dcca7293d083fe02b7a02be563d4c69a40d2285
fadhil <nurfadhil.kurniawan@ui.ac.id>
2026-09-08 21:23:29 +0700

    Menghubungkan view & template (part 1)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Nurfadhil",
+        "npm": "2506540765",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang percaya bahwa teknologi "
+            "dan pendidikan adalah kombinasi terbaik untuk menciptakan perubahan."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Nurfadhil",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
