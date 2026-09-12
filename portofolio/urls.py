4f61ae23ab3ced9b02885cf7a931bc6c775ac6a0
fadhil <nurfadhil.kurniawan@ui.ac.id>
2026-09-13 01:02:47 +0700

    menghubungkan navigasi antar halaman

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from main.views import show_main
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', show_main, name='show_main'),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls")),
 ]
