cf9a3f89919c4780e7964f05a214a1165239ae63
fadhil <nurfadhil.kurniawan@ui.ac.id>
2026-09-15 22:24:05 +0700

    feat: csrf token untuk form add projects

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -151,3 +151,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://pws.cs.ui.ac.id/nurfadhil.kurniawan/myportofolio"]
