6a0ab2dbd52cf3f89382c71a055adf5b5565df9b
fadhil <nurfadhil.kurniawan@ui.ac.id>
2026-09-15 22:22:56 +0700

    feat: Form tambah projects di forms, dengan menambah function serta class di models, urls, dan views

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skills
+from main.views import show_main, show_experience, show_skills, create_project, show_projects
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
-    path("skills/", show_skills, name="show_skills")
+    path("skills/", show_skills, name="show_skills"),
+    path("projects/", show_projects, name="show_projects"),
+    path("projects/add/", create_project, name="create_project"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from collections import OrderedDict
-from main.models import Experience, Skills
+from main.models import Experience, Skills, Project
+from main.forms import ProjectForm
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 
 def show_main(request):
@@ -40,3 +45,24 @@ def show_skills(request):
         "name" : "Nurfadhil",
         "grouped_skills": grouped}
     return render(request, 'skills.html', context)
+
+def show_projects(request):
+    context = {
+        "name": "Nurfadhil",
+        "project_list": Project.objects.all(),
+    }
+    return render(request, "Projects.html", context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Burhan",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
