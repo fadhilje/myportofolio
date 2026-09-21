8ff887e6d6e6ae454923731302d70337fee5ccb9
fadhil <nurfadhil.kurniawan@ui.ac.id>
2026-09-21 15:00:29 +0700

    feat(urls): tambah route create/update/delete/json Experience

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skills, create_project, show_projects, get_projects_json, delete_project
+from main.views import show_main, show_experience, create_experience, update_experience, delete_experience, get_experience_json, show_skills, show_projects, create_project, update_project, get_projects_json, delete_project
 
 app_name = "main"
 
 urlpatterns = [
-    path("", show_main, name="show_main"),
+     path("", show_main, name="show_main"),
+ 
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/update/", update_experience, name="update_experience"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
     path("skills/", show_skills, name="show_skills"),
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
+    path("projects/<uuid:project_id>/update/", update_project, name="update_project"),
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
-    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
